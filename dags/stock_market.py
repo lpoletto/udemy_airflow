2266d2fb1821b461b081647e0188250b576970af
Lautaro Poletto <lautaropoletto@gmail.com>
2025-08-04 13:04:53 +0000

    Feature: nueva task load_to_dw

--- dags/stock_market.py
+++ dags/stock_market.py
@@ -3,9 +3,12 @@ from airflow.hooks.base import BaseHook
 from airflow.sensors.base import PokeReturnValue
 from airflow.operators.python import PythonOperator
 from airflow.providers.docker.operators.docker import DockerOperator
+from astro import sql as aql
+from astro.files import File
+from astro.sql.table import Table, Metadata
 from datetime import datetime
 
-from include.stock_market.tasks import _get_stock_prices, _store_prices, _get_formatted_csv
+from include.stock_market.tasks import _get_stock_prices, _store_prices, _get_formatted_csv, BUCKET_NAME
 
 
 SYMBOL = "MELI"  # Symbol for Mercado Libre, Inc.
@@ -75,6 +78,27 @@ def stock_market():
         }
     )
 
-    is_api_available() >> get_stock_prices >> store_prices >> format_prices >> get_formatted_csv
+    load_to_dw = aql.load_file(
+        task_id="load_to_dw",
+        input_file=File(
+            path=f"s3://{BUCKET_NAME}/{{{{ ti.xcom_pull(task_ids='get_formatted_csv') }}}}",
+            conn_id="minio"
+        ),
+        output_table=Table(
+            name="stock_market",
+            conn_id="postgres",
+            metadata=Metadata(
+                schema="public"
+            )
+        ),
+        load_options={
+            "aws_access_key_id": BaseHook.get_connection("minio").login,
+            "aws_secret_access_key": BaseHook.get_connection("minio").password,
+            "endpoint_url": BaseHook.get_connection("minio").host
+        }
+    )
+
+
+    is_api_available() >> get_stock_prices >> store_prices >> format_prices >> get_formatted_csv >> load_to_dw
 
 stock_market()
